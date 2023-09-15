0989b011a389689a5bc9d427b1a4a46ada173579
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 <joseph.karadakis@gmail.com>
2023-09-15 01:28:24 -0700

    website2.0

--- websiteCode/script.js
+++ websiteCode/script.js
@@ -1,12 +1,13 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const tickerSelector = document.getElementById("tickerSelector");
     const newsContent = document.getElementById("news-content");
     const stockDataContent = document.getElementById("stock-data-content");
+    const candlestickChartContainer = document.getElementById("candlestickChartContainer");
+    const tickerImage = document.getElementById("tickerImage");
 
     // function to fetch news content
     function fetchNews(selectedTicker) {
-        const apiKey =  'demo';
+        const apiKey = 'your_api_key'; // Replace with your actual API key
         const apiUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${selectedTicker}&apikey=${apiKey}`;
 
         // loading message
@@ -30,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
                             <h5>${article.title}</h5>
                             <p>${article.summary}</p>
                             <small>${articleDate.toLocaleString()}</small>
-                            <a href="${article.url}" target="_blank" class="btn btn-primary btn-sm float-right">Read More</a>
+                            <a href="${article.url}" target="_blank" class="btn custom-box-bg btn-sm text-white float-right">Read More</a>
                         `;
                         newsList.appendChild(listItem);
                     });
@@ -53,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // fetch and display stock data
     function fetchStockData(selectedTicker) {
-        const apiKey =  'demo'; 
+        const apiKey = 'your_api_key'; // Replace with your actual API key
         const stockApiUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${selectedTicker}&apikey=${apiKey}`;
 
         stockDataContent.innerHTML = '<div class="text-center">Loading stock data...</div>';
@@ -88,16 +89,10 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     }
 
-    // Listener for tickerSelector
-    tickerSelector.addEventListener("change", function () {
-        const selectedTicker = this.value;
-        fetchNews(selectedTicker);
-        fetchStockData(selectedTicker);
-    });
     // Function to fetch and display candlestick chart
     function fetchCandlestickChart(selectedTicker) {
-        const apiKey = 'demo';
-        const intradayApiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${apiKey}`;
+        const apiKey = 'your_api_key'; // Replace with your actual API key
+        const intradayApiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${selectedTicker}&interval=5min&apikey=${apiKey}`;
 
         fetch(intradayApiUrl)
             .then((response) => response.json())
@@ -139,7 +134,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 // You can display an error message or handle this case as needed.
             });
     }
+    // Create a map to store ticker symbols and their corresponding thumbnail URLs
+    const tickerThumbnailUrls = new Map();
 
+    // Add entries for each ticker symbol and its thumbnail URL
+    tickerThumbnailUrls.set("AAPL", "https://mdc.mo.gov/sites/default/files/styles/carousel/public/mdcd7/media/images/2014/03/pa2-04-2014.jpg?h=ed80be45&itok=ROS0jacl");
+    tickerThumbnailUrls.set("SBUX", "https://example.com/thumbnails/SBUX.png");
+    tickerThumbnailUrls.set("MSFT", "https://example.com/thumbnails/MSFT.png");
+    tickerThumbnailUrls.set("CSCO", "https://example.com/thumbnails/CSCO.png");
+    tickerThumbnailUrls.set("QCOM", "https://example.com/thumbnails/QCOM.png");
+    tickerThumbnailUrls.set("META", "https://example.com/thumbnails/META.png");
+    tickerThumbnailUrls.set("AMZN", "https://example.com/thumbnails/AMZN.png");
+    tickerThumbnailUrls.set("TSLA", "https://example.com/thumbnails/TSLA.png");
+    tickerThumbnailUrls.set("AMD", "https://example.com/thumbnails/AMD.png");
+    tickerThumbnailUrls.set("NFLX", "https://example.com/thumbnails/NFLX.png");
+
+    // Function to get the thumbnail URL for a selected ticker
+    function getTickerThumbnailUrl(predictionsSelectedTicker) {
+        // Check if the selectedTicker exists in the map
+        if (tickerThumbnailUrls.has(predictionsSelectedTicker)) {
+            return tickerThumbnailUrls.get(predictionsSelectedTicker);
+        } else {
+            // Return a default URL or handle the case when the ticker is not found
+            return "https://example.com/default-thumbnail.png"; // Replace with a default image URL
+        }
+    }
     // Listener for tickerSelector
     tickerSelector.addEventListener("change", function () {
         const selectedTicker = this.value;
@@ -147,5 +166,16 @@ document.addEventListener("DOMContentLoaded", function () {
         fetchStockData(selectedTicker);
         fetchCandlestickChart(selectedTicker); // Fetch and display the candlestick chart
     });
-    
+
+    // Listener for predictionsTickerSelector
+    const predictionsTickerSelector = document.getElementById("predictionsTickerSelector");
+    predictionsTickerSelector.addEventListener("change", function () {
+        const predictionsSelectedTicker = this.value;
+        getTickerThumbnailUrl(predictionsSelectedTicker);
+        
+        // Display the ticker thumbnail based on the selected ticker
+        const thumbnailUrl = getTickerThumbnailUrl(predictionsSelectedTicker);
+        tickerImage.innerHTML = `<img src="${thumbnailUrl}" alt="${predictionsSelectedTicker} Thumbnail" class="img-thumbnail">`;
+    });
+
 });
