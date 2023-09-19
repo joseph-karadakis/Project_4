83be60cb4daed2dbdcba406b6bb84df47f823fe3
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 <joseph.karadakis@gmail.com>
2023-09-18 21:36:11 -0700

    removing assets

--- websiteCode/script.js
+++ websiteCode/script.js
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to load and parse the JSON file for the selected ticker symbol
     function loadJSONFile(predictionsSelectedTicker) {
-        const jsonFileName = `../machineLearning/josephsLNRexperiment/${predictionsSelectedTicker}_model_info.json`;
+        const jsonFileName = `../machineLearning/LinReg/${predictionsSelectedTicker}_model_info.json`;
 
         fetch(jsonFileName)
             .then((response) => response.json())
             .then((data) => {
-                if (data && data.ticker && data.intercept !== undefined && data.coef !== undefined) {
+                if (data && data.intercept !== undefined && data.coef !== undefined) {
                     jsonData = data; 
                 } else {
                     throw new Error("JSON data is missing required fields.");
@@ -225,211 +225,11 @@ document.addEventListener("DOMContentLoaded", function () {
         // Display the ticker thumbnail based on the selected ticker
         tickerImage.innerHTML = `<img src="${thumbnailPath}" alt="${predictionsSelectedTicker} Thumbnail" class="img-thumbnail">`;
 
-        // Define text based on the selected ticker
-        let text = "";
-        switch(predictionsSelectedTicker) {
-            case "AAPL":
-                text = `
-                <pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy: 0.5321559725277262
-                Validation Accuracy: 0.5278904665314402
-                
-                SVC:
-                Training Accuracy: 0.533309388603404
-                Validation Accuracy: 0.4947388438133874
-                
-                XGBClassifier:
-                Training Accuracy: 0.9617975919113765
-                Validation Accuracy: 0.5347996957403651`;
-                break;
-            case "AMD":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5123858688017979
-                Validation Accuracy :  0.5599015834963094
-                
-                SVC:
-                Training Accuracy :  0.4842666729097408
-                Validation Accuracy :  0.43249637246861394
-
-                
-                XGBClassifier:
-                Training Accuracy :  0.9235586302695447
-                Validation Accuracy :  0.5212920320484512`;
-                break;
-            case "AMZN":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5209906930717874
-                Validation Accuracy :  0.5328575102880659              
-                
-                SVC:
-                Training Accuracy :  0.4836377187378653
-                Validation Accuracy :  0.5009002057613169                
-              
-                XGBClassifier:
-                Training Accuracy :  0.9645560889607054
-                Validation Accuracy :  0.5176826131687242`;
-                break;
-            case "CSCO":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.517419898371889
-                Validation Accuracy :  0.5168610547667343                              
-                
-                SVC:
-                Training Accuracy :  0.4971664995768098
-                Validation Accuracy :  0.4851990365111562                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.921490019894624
-                Validation Accuracy :  0.4806668356997971`;
-                break;
-            case "META":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5418181547730888
-                Validation Accuracy :  0.5128383506197824                              
-                
-                SVC:
-                Training Accuracy :  0.5406277792391616
-                Validation Accuracy :  0.5373134328358209                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.9477961024889456
-                Validation Accuracy :  0.5576460915760182`;
-                break;
-            case "MSFT":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5400376351657981
-                Validation Accuracy :  0.4927104462474645                              
-                
-                SVC:
-                Training Accuracy :  0.45707595887517016
-                Validation Accuracy :  0.5067190669371197                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.9321757194497912
-                Validation Accuracy :  0.5402827079107505`;
-                break;
-            case "NFLX":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5336983575472309
-                Validation Accuracy :  0.518733809313199                              
-                
-                SVC:
-                Training Accuracy :  0.5318466835824229
-                Validation Accuracy :  0.49804132179187466                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.9326997343929501
-                Validation Accuracy :  0.5071081064004549`;
-                break;
-            case "QCOM":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5203775954218742
-                Validation Accuracy :  0.5455350366810018                              
-                
-                SVC:
-                Training Accuracy :  0.5278050235684767
-                Validation Accuracy :  0.5439855805717176                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.9408699095057961
-                Validation Accuracy :  0.47514545914495315`;
-                break;
-            case "SBUX":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5178261348430986
-                Validation Accuracy :  0.4579107505070994                              
-                
-                SVC:
-                Training Accuracy :  0.5282666991616147
-                Validation Accuracy :  0.42450557809330625                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.9417317151474108
-                Validation Accuracy :  0.4757543103448276`;
-                break;
-            case "TSLA":
-                text = `<pre style="color: white;">
-
-
-                The training and validation 
-                accuracy scores of our 3 models is:
-
-                LogisticRegression:
-                Training Accuracy :  0.5140954819427772
-                Validation Accuracy :  0.4810383032605255                              
-                
-                SVC:
-                Training Accuracy :  0.4942828693977592
-                Validation Accuracy :  0.5406141183918962                               
-                
-                XGBClassifier:
-                Training Accuracy :  0.9371096094687875
-                Validation Accuracy :  0.40443178220956`;
-                break;
-            default:
-                text = "No information available for this ticker.";
-        }
-
-        // Display the text next to the ticker image
-        tickerText.innerHTML = text;
-
+        
         // Load and parse the JSON file for the selected ticker symbol
         loadJSONFile(predictionsSelectedTicker);
     });
+
     // Attach a click event listener to the predictButton
     const predictButton = document.getElementById("predictButton");
     predictButton.addEventListener("click", function () {
